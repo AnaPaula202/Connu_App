baf49b13d5ae9bcc9308e706153ee6da21973083
202060230 <202060230@ucc.mx>
2023-11-12 01:45:26 -0600

    docs: añade las demás pantallas

---
+++ app/src/main/java/com/example/connu/MainPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.connu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}

024d6140b9647813dc4a09a3aa36d7df0a1c7041
202060230 <202060230@ucc.mx>
2023-11-15 12:25:05 -0600

    style: detalla pantallas de inicio de sesion y de registro

--- app/src/main/java/com/example/connu/RegisterActivity.kt
+++ app/src/main/java/com/example/connu/RegisterActivity.kt
@@ -1,11 +1,29 @@
 package com.example.connu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val tvGoback : TextView = findViewById(R.id.tvGoback)
+        val ivGoback : ImageView = findViewById(R.id.ivGoback)
+
+        tvGoback.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        ivGoback.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
